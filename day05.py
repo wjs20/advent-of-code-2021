3e3a72f5f5590c369ec89dae3b8caf4b6addd215
William Steele <johnsteele2009@live.co.uk>
2021-12-18 17:14:14 +0000

    added solution for day05 part 2

--- day05.py
+++ day05.py
@@ -1,5 +1,6 @@
 from itertools import chain
 import numpy as np
+from numpy.lib.shape_base import row_stack
 from utils import get_input
 
 class VentLine:
@@ -18,23 +19,27 @@ class VentLine:
     def positions(self):
         
         if self.is_vertical():
-            x = self.x1
-            if self.y2 > self.y1:
-                y = list(range(self.y1, self.y2 + 1))
-            else:
-                y = list(range(self.y2, self.y1 + 1))
-            return x, y
+            col = self.x1
+            row = sorted([self.y1, self.y2])
+            row = list(range(row[0], row[1] + 1))
+            return row, col
         
-        elif self.is_horizontal():
-            y = self.y1
-            if self.x2 > self.x1:
-                x = list(range(self.x1, self.x2 + 1))
-            else:
-                x = list(range(self.x2, self.x1 + 1))
-            return x, y
+        if self.is_horizontal():
+            col = sorted([self.x1, self.x2])
+            row = self.y1
+            col = list(range(col[0], col[1] + 1))
+            return row, col
         
         else:
-            raise ValueError('diagonal line')
+            if self.x1 > self.x2:
+                col = list(range(self.x1, self.x2 - 1, -1))
+            else:
+                col = list(range(self.x1, self.x2 + 1))
+            if self.y1 > self.y2:
+                row = list(range(self.y1, self.y2 - 1, -1))
+            else:
+                row = list(range(self.y1, self.y2 + 1))
+            return row, col
 
     @staticmethod
     def parse(raw):
@@ -50,17 +55,33 @@ class SeaBed:
         self.ventlines = ventlines
 
     def update_map(self, ventline):
-        x, y = ventline.positions()
-        self.map[x, y] += 1
+        row, col = ventline.positions()
+        self.map[row, col] += 1
 
     def count_overlaps(self):
         return (self.map >= 2).sum()
 
-    def map_seabed(self):
-        for vl in self.ventlines:
-            if vl.is_horizontal() or vl.is_vertical():
+    def map_seabed(self, ignore_diags = True):
+
+        self.reset()
+
+        if ignore_diags:
+            for vl in self.ventlines:
+                if vl.is_horizontal() or vl.is_vertical():
+                    self.update_map(vl)
+        
+        else:
+            for vl in self.ventlines:
                 self.update_map(vl)
+
+    def danger_spots(self):
+        if self.map.sum() == 0:
+            raise AssertionError ('Seabed unmapped')
+
         return (self.map >= 2).sum()
+    
+    def reset(self):
+        self.map[:] = 0
 
     @staticmethod
     def parse(ventlines):
@@ -78,7 +99,7 @@ class SeaBed:
         xmax = max(xs)
         ymax = max(ys)
 
-        map = np.zeros((xmax+1, ymax+1))
+        map = np.zeros((xmax+2, ymax+2))
         return SeaBed(map, ventlines)
         
 if __name__ == '__main__':
@@ -95,11 +116,14 @@ if __name__ == '__main__':
 
     ventlines = [VentLine.parse(line) for line in example.splitlines()]
     assert ventlines[0].x1 == 0, 'ventline error'
-    assert ventlines[5].y1 == 4, 'ventline error'
     seabed = SeaBed.parse(ventlines)
     seabed.map_seabed()
-    assert (seabed.map >= 2).sum() == 5
+    assert seabed.danger_spots() == 5
+    seabed.map_seabed(ignore_diags=False)
+    assert seabed.danger_spots() == 12
     raw = get_input('day05.txt')
     ventlines = [VentLine.parse(line) for line in raw.splitlines()]
     seabed = SeaBed.parse(ventlines)
-    print(seabed.map_seabed())
+    print(seabed.map.shape)
+    seabed.map_seabed(ignore_diags=False)
+    print(seabed.danger_spots())
